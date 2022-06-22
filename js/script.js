eefb31f0d1d120347fa8c8d5dfe64ce24b1361e7
Alexandra Krasnova <krasnova@physics.msu.ru>
2022-06-23 01:02:49 +0400

    Added calculator functionality

--- js/script.js
+++ js/script.js
@@ -313,7 +313,7 @@ window.addEventListener('DOMContentLoaded', () => {
     slider.style.position = 'relative';
 
     const indicators = document.createElement('ol'),
-          dots = [];
+        dots = [];
     indicators.classList.add('carousel-indicators');
     slider.append(indicators);
 
@@ -382,4 +382,112 @@ window.addEventListener('DOMContentLoaded', () => {
     function setTotal() {
         total.textContent = addZeroIfNeeded(slides.length);
     }
+
+    // Calculator
+
+    const result = document.querySelector('.calculating__result span');
+    let age, height, weight, sex, ratio;
+
+    if (localStorage.getItem('sex')) {
+        sex = localStorage.getItem('sex');
+    } else {
+        sex = 'female';
+        localStorage.setItem('sex', sex);
+    }
+
+    if (localStorage.getItem('ratio')) {
+        ratio = localStorage.getItem('ratio');
+    } else {
+        ratio = 1.375;
+        localStorage.setItem('ratio', ratio);
+    }
+
+    function initLocalSettngs(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(element => {
+            element.classList.remove(activeClass);
+            if (element.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+                element.classList.add(activeClass);
+            } else if (element.getAttribute('id') === localStorage.getItem('sex')) {
+                element.classList.add(activeClass);
+            }
+        });
+    }
+
+    initLocalSettngs('.calculating__choose_big div', 'calculating__choose-item_active');
+    initLocalSettngs('#gender div', 'calculating__choose-item_active');
+
+    function caltTotal() {
+        if (!age || !sex || !height || !weight || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'male') {
+            result.textContent = Math.round(ratio * (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age));
+        } else {
+            result.textContent = Math.round(ratio * (447.6 + 9.2 * weight + 3.1 * height - 4.3 * age));
+        }
+
+    }
+
+    caltTotal();
+
+    function getStaticInformation(selector, activeClass) {
+        const elements = document.querySelectorAll(selector);
+
+        elements.forEach(element => {
+            element.addEventListener('click', () => {
+                if (element.getAttribute('data-ratio')) {
+                    ratio = +element.getAttribute('data-ratio');
+                    localStorage.setItem('ratio', ratio);
+                } else {
+                    sex = element.getAttribute('id');
+                    localStorage.setItem('sex', sex);
+                }
+
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+
+                element.classList.add(activeClass);
+
+                caltTotal();
+            });
+        });
+    }
+
+    getStaticInformation('#gender div', 'calculating__choose-item_active');
+
+    getStaticInformation('.calculating__choose_big div', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            if (input.value.match(/\D/g)) {
+                input.style.border = '1px solid red';
+            } else {
+                input.style.border = 'none';
+            }
+
+            switch (input.getAttribute('id')) {
+                case 'height':
+                    height = +input.value;
+                    break;
+                case 'weight':
+                    weight = +input.value;
+                    break;
+                case 'age':
+                    age = +input.value;
+                    break;
+            }
+            caltTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 });
